200b03de4f77d31bdb3a1b9da537d8ad9be1f9c3
Guangyu <everyting-day@foxmail.com>
2020-09-14 23:19:08 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
+import x from "./x.js";
 console.log("hi");
 console.log("天了噜");
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,24 @@
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 var path = require("path");
 
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].[contenthash].js",
+    filename: "index.[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "FFAA-天了噜",
+      template: "src/assets/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
